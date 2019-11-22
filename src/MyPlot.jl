e9a16eee85f13551acf4d988f916ab14f85c9db8
bingcheng1998 <bingcheng1998@qq.com>
2019-11-22 16:34:49 +0800

    新建全部数据绘图
    将*所有*的函数打包到`MyPlot.jl`, 方便引用

--- src/MyPlot.jl
+++ src/MyPlot.jl
@@ -41,4 +41,42 @@ function PicAll3(P,W,G,color = :inferno,markersize = 1,size_x = 5, title="", tit
     return p4
 end
 
+function get_weight(X,Y,Weight)
+    P_trip = []
+    kk = maximum(Weight)
+    for i = 0:kk
+        current_trip = dataSplit.ListDataSplite([X,Y,Weight],Weight,i);
+        push!(P_trip,current_trip)
+    end
+    return P_trip
+end
+
+function getWeightPic(X,Y,Weight, color = :inferno, markersize = 1,size_x = 5,title = "",draw_0 = true)
+    ## color is a list of color, you should change the color and data
+    ## you can find colors at: http://docs.juliaplots.org/latest/colors/#misc
+    markersize = markersize*size_x
+    kk = Int(maximum(Weight))
+    P_W = get_weight(X,Y,Weight)
+    colors = RGBA[cgrad(color)[z] for z=range(0,stop=1,length=kk)]
+    pic = scatter()
+    # second, plot the non-apple graph
+    i = 1
+    color_s = :azure3
+    if draw_0
+        pic = scatter!(P_W[i][1], P_W[i][2], markersize = markersize, markerstrokewidth = 0,
+            markercolor = color_s, size=(107*size_x,107*size_x), leg=false, xlims = (0,107), xticks = 0:10:107,
+            ylims = (0,107), yticks = 0:10:107,label=i-1,α=0.3,title = title)
+    end
+    # First, plot the heat graph
+    for i = 2:kk
+        color_s = colors[i]
+        pic = scatter!(P_W[i][1], P_W[i][2], markersize = markersize, markerstrokewidth = 0,
+            markercolor = color_s, size=(107*size_x,107*size_x), leg=false, xlims = (0,107), xticks = 0:10:107,
+            ylims = (0,107), yticks = 0:10:107,label=i-1,zcolor = i-1,m=(color, 0.8))
+    end
+    
+    return pic
+end
+
+
 end
